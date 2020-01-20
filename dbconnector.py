d7bf1e72e69e397cfce8942e5168687a4ef140a3
Anatoli Babenia <anatoli@rainforce.org>
2020-01-20 15:05:51 +0300

    Debug DATABASE_URL values on Travis

--- dbconnector.py
+++ dbconnector.py
@@ -50,5 +50,6 @@ if __name__ == "__main__":
         + "{db_name}"
         + "?driver=FreeTDS&autocommit=True"
     ) """
+    print('env DATABASE_URL=' + DATABASE_URL)
     with DBConnector("master") as conn:
         print(conn)
