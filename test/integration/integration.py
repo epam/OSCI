dbf0242633c025e52adb87cc538b9a193d133866
Anatoli Babenia <anatoli@rainforce.org>
2020-01-25 15:56:07 +0300

    .travis.yml Run integration tests

--- test/integration/integration.py
+++ test/integration/integration.py
@@ -25,7 +25,9 @@ from sql_runner import run_query_from_file
 
 
 class IntegrationTest(unittest.TestCase):
-    FIXTURE_FOLDER = 'fixtures'
+    CWD = os.path.abspath(os.path.dirname(__file__))
+    FIXTURE_FOLDER = CWD + '/fixtures'
+    QUERY_FOLDER = os.path.abspath(CWD + "../../../SQL_queries/service_queries")
     TMP_FOLDER = 'resources'
     TEST_DB_NAME = 'test_db'
 
@@ -35,8 +37,7 @@ class IntegrationTest(unittest.TestCase):
             os.mkdir(cls.TMP_FOLDER)
         path = os.path.join(cls.FIXTURE_FOLDER, 'create_test_db.sql')
         run_query_from_file('master', path)
-        path = os.path.join(os.path.abspath(os.path.join("..", os.pardir)), 'SQL_queries', 'service_queries',
-                            'create_empty_tables.sql')
+        path = os.path.join(cls.QUERY_FOLDER, 'create_empty_tables.sql')
         run_query_from_file(database=cls.TEST_DB_NAME, path_to_file=path)
 
     def _unzip_test_data(self):
@@ -47,11 +48,9 @@ class IntegrationTest(unittest.TestCase):
         self._unzip_test_data()
         path = os.path.join(self.TMP_FOLDER)
         upload_files_from_directory(path, database=self.TEST_DB_NAME)
-        path = os.path.join(os.path.abspath(os.path.join("..", os.pardir)), 'SQL_queries', 'service_queries',
-                            'create_filtered_table.sql')
+        path = os.path.join(self.QUERY_FOLDER, 'create_filtered_table.sql')
         run_query_from_file(database=self.TEST_DB_NAME, path_to_file=path)
-        path = os.path.join(os.path.abspath(os.path.join("..", os.pardir)), 'SQL_queries', 'reports',
-                            'top_commits_ranking.sql')
+        path = os.path.join(self.QUERY_FOLDER, 'top_commits_ranking.sql')
         result = run_query_from_file(database=self.TEST_DB_NAME, path_to_file=path)
         self.assertEqual(tuple(result[0]), ('Huawei', 2))
 
